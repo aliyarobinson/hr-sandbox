879f05d1892ed80bf51cc9727bdc8267f102c696
Aliya Robinson <aliya@chartbeat.com>
2016-02-17 07:50:50 -0500

    back button works on hashchange. #index dosen't work on load

--- js/script.js
+++ js/script.js
@@ -7,14 +7,36 @@ var HR = HR || {};
     pages: [],
     currPageArr: location.pathname.split('/'),
     currPage: location.pathname.split('/').pop().split('.')[0],
+    currHash: location.hash.split('#').pop(),
     baseURL: location.origin,
 
     init: function () {
       HR.getPages();
       HR.preload();
-      HR.homeAnim();
+      // HR.homeAnim();
+      // HR.updateContent(HR.currPage);
       console.log('currPageArr: ', HR.currPageArr);
       console.log('currPage: ', HR.currPage);
+
+      if (HR.currPage === '' || HR.currHash === 'index'){
+        console.log('init on load blank path');
+        HR.currPage = 'index';
+      }
+      HR.updateContent(HR.currPage);
+      HR.homeAnim();
+
+      // $( window ).load(function() {
+      //   console.log('*******************************');
+      //   console.log('on load fired');
+
+      //   if (HR.currPage === ''){
+      //     console.log('init on load blank path');
+      //     HR.currPage = 'index';
+      //   }
+      //   HR.updateContent(HR.currPage);
+      // });
+
+      window.onhashchange = HR.locationHashChanged;
 
       /**************************************/
       /*   Site Nav button animation on click
@@ -42,26 +64,33 @@ var HR = HR || {};
         }
       }); // end on click .site-nav-btn
 
+
+
       /**************************************/
       /*   Navigation Content Switch Animation on click
       /***************************************************/
-      $('.site-nav .list-nav a').on('click', function (e) {
+      // $('.site-nav .list-nav a').on('click', function (e) {
+      $('[data-content]').on('click', function (e) {
         e.preventDefault();
         console.log('clicked nav link');
-        var linkName = $(this).attr('href').split('.')[0];
-        var pageClass = '.' + linkName + '-page';
-        console.log('linkName: ', linkName);
-        $('body').attr('class','');
-        $('body').attr('class',linkName);
-        // $('.content-container').hide();
-        $('.content-holder .wrapper').contents().remove();
-        var pageClone = $(pageClass).contents().clone();
-        $('.content-holder .wrapper').append(pageClone);
-
-        // location.pathname = 'other/hakim/hakim-minimal/' + linkName + '.html';
-
-
+        var linkName = $(this).data('content');
+        HR.updateContent(linkName);
       });
+
+      // $(document).pjax('[data-content]', '.content-holder');
+    },
+
+    locationHashChanged: function() {
+      if ("onhashchange" in window) {
+        console.log('*******************************');
+        console.log('locationHashChanged fired');
+        currHash = location.hash.split('#').pop();
+
+        if (currHash === ''){
+          currHash = 'index';
+        }
+        HR.updateContent(currHash);
+      }
     },
 
     getPages: function() {
@@ -76,8 +105,8 @@ var HR = HR || {};
       // for each page create an element with contents from the page
       for (var i = 0; i < HR.pages.length; i++) {
         var pages = HR.pages;
-        // var pageBaseURL = "http://localhost:8888/other/hakim/hakim-minimal/";
-        var pageBaseURL = "http://aliyarobinson.github.io/hakimrobinson/";
+        var pageBaseURL = "http://localhost:8888/other/hakim/hakim-minimal/";
+        // var pageBaseURL = "http://aliyarobinson.github.io/hakimrobinson/";
 
         // SHOULD CREATE A DOC FRAGEMENT HERE BEFORE LAUNCH
 
@@ -112,56 +141,40 @@ var HR = HR || {};
         },300);
         // curr_img.attr('xlink:href', '../intro_imgs/img_'+img_counter+'.jpg' );
       }
+    },
+
+    updateContent: function (pageName) {
+      var linkName = pageName;
+      var pageClass = '.' + linkName + '-page';
+      console.log('linkName: ', linkName);
+      $('body').attr('class','');
+      $('body').attr('class',linkName);
+      // $('.content-container').hide();
+      $('.content-holder .wrapper').contents().remove();
+      var pageClone = $(pageClass).contents().clone();
+      $('.content-holder .wrapper').append(pageClone);
+      location.hash = '#' + linkName;
+
+      // location.pathname = 'other/hakim/hakim-minimal/' + linkName + '.html';
     }
-
   };
-
-
-
-
-
-  // find .content-containter -  make display:none;
-  // find page by url
-  // trigger click of navigation
-
- 
-
-
-  
-
-  /**************************************/
-  /*   Navigation Content Switch Animation on click
-  /***************************************************/
-  // $('.site-nav .list-nav a').on('click', function (e) {
-  //   e.preventDefault();
-
-  //   var linkURL = $(this).attr('href');
-  //   var pageURL = "http://localhost:8888/other/hakim/hakim-minimal/" + linkURL;
-
-  //   /* Create container for new page contents  */
-
-  //   $('.main-content .wrapper').append('<div class="content-container" />')
-
-  //   /* Load new page contents  */
-
-  //   $( ".content-container:nth-child(2)" ).load( pageURL + " .content-container section" );
-
-  //   /*   */
-
-
-
-  //   // get page name from url
-  //   // find .content-containter - 
-  //   // create a new .content-container under it as a sibling
-  //   // ajax load second .content-container with page content
-  //   // remove first container
-  //   // fade in second container
-
-  //   $('.content-container').animate({
-
-  //   }, 1000);
-
-  // })
 })(jQuery); // end SEF
 
-HR.init();
+// HR.init();
+
+window.onload = function() {
+  console.log('onload');
+  HR.init();
+
+  // // if (!supports_history_api()) { return; }
+  // window.setTimeout(function() {
+  //   // window.addEventListener("popstate", function(e) {
+  //     console.log('popstate');
+  //     HR.updateContent(HR.currPage);
+  //   // }, false);
+  // }, 1);
+}
+
+function supports_history_api() {
+  return !!(window.history && history.pushState);
+}
