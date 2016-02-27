a54e781474d9f5fbb0703dbc2e6a925fd69ca848
aliya robinson <aliya robinson>
2016-02-26 19:25:18 -0500

    began creating fixed responsive nav

--- js/script.js
+++ js/script.js
@@ -13,9 +13,65 @@ var HR = HR || {};
       HR.updatePageName();
       // HR.prepTransition();
       // HR.transitionContent();
+      
+
+      /**************************************/
+      /*   Handeling Font issues in Windows
+      /***************************************************/
+      var OSName="Unknown OS";
+      if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
+      if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
+      if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
+      if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
+
+      if (OSName === "Windows") 
+      {
+        $('p').css('font-wieght',800);
+      } 
+
+
+
+      /**************************************/
+      /*   Preping Videos
+      /***************************************************/
+      HR.createVidArr();
+      HR.createVidPoster();
+      HR.bindVidEvent();
+
+
+      $(".video").each(function() {
+        var vidID = $(this).attr('id');
+        console.log('youtube id: ', vidID);
+
+        // Based on the YouTube ID, we can easily find the thumbnail image
+        $(this).css('background-image', 'url(http://i.ytimg.com/vi/' + this.id + '/hqdefault.jpg)');
+    
+        // Overlay the Play icon to make it look like a video player
+        $(this).append($('<div/>', {'class': 'play'}));
+    
+        // $(document).delegate('#'+vidID, 'click', function() {
+        $(this).find('.play').on('click', function(e) {
+            console.log('video');
+            // Create an iFrame with autoplay set to true
+            var iframe_url = "https://www.youtube.com/embed/" + this.id + "?autoplay=1&autohide=1";
+            if ($(this).data('params')) iframe_url+='&'+$(this).data('params');
+    
+            // The height and width of the iFrame should be the same as parent
+            var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': $(this).width(), 'height': $(this).height() })
+    
+            // Replace the YouTube thumbnail with YouTube HTML5 Player
+            $(this).replaceWith(iframe);
+        });
+      });
         
+
+      /**************************************/
+      /*   Binding load and hashchange events
+      /***************************************************/  
       window.onhashchange = HR.locationHashChanged;
       $(window).on('load', HR.updatePageContent );
+
+
 
       /**************************************/
       /*   Site Nav button animation on click
@@ -84,6 +140,18 @@ var HR = HR || {};
         console.log('videosLoaded');
         return true;
       });
+    },
+
+    createVidArr: function (vids){
+      // for each .video, get the data-id and place in the HR.vidArr property
+    },
+
+    createVidPoster: function (vidArr) {
+      // for each vidArr element append an a tag that goes to the video url and to images inside of the a tag (the postser and the play button(pointer-events: none)
+    },
+
+    bindVidEvent: function (vidArr) {
+      // for each vidArr a tag preventDefault, then load iframe video under images, then fade out images, then place images under video or remove images
     },
 
     transitionContent: function(page){
