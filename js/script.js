fb7ff7a1ceebbeb12114689699d3267423e9b269
aliya robinson <aliya robinson>
2016-03-28 19:33:54 -0400

    removed some old code

--- js/script.js
+++ js/script.js
@@ -24,34 +24,12 @@ var HR = HR || {};
         });
             
 
-      /**************************************/
-      /*   Handeling Font issues in Windows
-      /***************************************************/
-      var OSName="Unknown OS";
-      if (navigator.appVersion.indexOf("Win")!=-1) OSName="Windows";
-      if (navigator.appVersion.indexOf("Mac")!=-1) OSName="MacOS";
-      if (navigator.appVersion.indexOf("X11")!=-1) OSName="UNIX";
-      if (navigator.appVersion.indexOf("Linux")!=-1) OSName="Linux";
-
-      if (OSName === "Windows") 
-      {
-        $('p').css('font-wieght',800);
-      } 
-
+      
       /**************************************/
       /*   Video Click image swap
       /***************************************************/
       $(document).on( HR.clickHandler, '.video', function() { 
         console.log('!!!'); 
-        var vidID = $(this).attr('id');
-
-        var iframe_url = "https://www.youtube.com/embed/" + vidID + "?rel=0&autoplay=1&autohide=1&showinfo=0";
-    
-        // The height and width of the iFrame should be the same as parent
-        var iframe = $('<iframe/>', {'frameborder': '0', 'allowfullscreen': '', 'src': iframe_url });
-
-        // Replace the YouTube thumbnail with YouTube HTML5 Player
-        $(this).append(iframe);
       });
 
       /**************************************/
@@ -61,14 +39,6 @@ var HR = HR || {};
         console.log('*****************onpopstate triggered*********************');
         var thisPage = location.href.split('/')[location.href.split('/').length -1 ];
         var thisPageName = thisPage.replace('.html','');
-        console.log('popstate - thisPage: ', thisPage);
-        if (thisPage === ""){
-          thisPageName = 'index';
-        }
-
-        HR.updatePageName();
-
-        HR.transitionContent(thisPageName);
       }
 
       /**************************************/
@@ -78,18 +48,11 @@ var HR = HR || {};
         e.preventDefault();
         console.log('*****************nav click*********************');
 
-        // if(typeof History === "function") {
         if( History ) {
           console.log('history!');
 
           var href = ($(this).attr('href')) ? $(this).attr('href') : $(this).attr('xlink:href');
           var thisPage = href.replace('.html','');
-
-          history.pushState({page: thisPage}, null, href);
-
-          HR.updatePageName();
-
-          HR.transitionContent(thisPage);
         }
       });
 
@@ -109,12 +72,7 @@ var HR = HR || {};
       /*   Binding load event
       /***************************************************/  
       $(window).on('load', function(){
-        // $('.loader').css({
-        //   'display': 'block',
-        //   'opacity': 1
-        // });
 
-        HR.updatePageContent();
       });
 
 
@@ -124,128 +82,12 @@ var HR = HR || {};
       /***************************************************/
       $('.site-nav-btn').on( HR.clickHandler , function(){
         $(this).toggleClass('active');
-        if($('.site-nav').hasClass('expanded')){
-          $('.site-nav').velocity({
-            'margin-left': '-320px'
-          },600);
-          $('.site-nav-bkgd').velocity({
-            'left': '-150%',
-            'opacity': 0
-          },600);
-          $('.site-nav').removeClass('expanded');
-        } else {
-          $('.site-nav').velocity({
-            'margin-left':0
-          },600);
-          $('.site-nav-bkgd').velocity({
-            'left': '-50%',
-            'opacity': 1
-          },600);
-          $('.site-nav').addClass('expanded');
-        }
+        
       }); // end on click .site-nav-btn
 
     },
     
-    updatePageName: function() {
-        console.log('*****************updatePageName*********************');
 
-        var urlPathArr = location.pathname.split('/');
-        var urlPageStr = urlPathArr[urlPathArr.length - 1];
-        console.log('urlPathArr: ', urlPathArr);
-        console.log('urlPageStr: ', urlPageStr);
-
-        if(urlPageStr === '' || null){
-          HR.currPageName = 'index';
-          console.log('updatePageName - name: ', HR.currPageName);
-
-          return;
-        }
-
-        HR.currPageName = urlPageStr.replace('.html','');
-
-        
-        console.log('updatePageName - name: ', HR.currPageName);
-    },
-
-    imagesLoaded: function() {
-      $('image').load(function() {
-        console.log('imagesLoaded');
-        return true;
-      });
-    },
-
-    swapImgVid: function(vidID, context) {
-      console.log('swapImgVid');
-      var iframe_url = "https://www.youtube.com/embed/" + vidID + "?autoplay=1&autohide=1";
-    
-      // The height and width of the iFrame should be the same as parent
-      var iframe = $('<iframe/>', {'frameborder': '0', 'src': iframe_url, 'width': '100%', 'height': '300px' })
-
-      // Replace the YouTube thumbnail with YouTube HTML5 Player
-      context.append(iframe);
-    },
-
-    transitionContent: function(page){
-        console.log('*****************transitionContent*********************');
-
-        console.log('transitionContent - name: ', HR.currPageName);
-        // HR.imagesLoaded();
-        if (HR.imagesLoaded){
-          if(page === 'index'){
-            // $('.loader').addClass('collapsed');
-            HR.homeAnim();
-          }
-        }
-          
-        $( "#content-holder" ).load( page + ".html .content-container section" );
-
-        $('body').attr('class', '').addClass(page + '-page');
-
-        // $('.loader').animate({
-        //   'opacity':0
-        // },400)
-        // .css({
-        //   'display': 'none'
-        // });
-
-        $('#content-holder').animate({
-          opacity: 1
-        },600);
-    },
-    
-    homeAnim: function() {
-      /**************************************/
-      /*   Home Intro Image Animation
-      /***************************************************/
-      var img_counter = 6;
-      var curr_img = $('.lg-logo image');
-      var imgInterval = setInterval(function(){ 
-        animateImgs();
-        if (img_counter === 0){
-          clearInterval(imgInterval);
-        }
-      }, 600);
-
-      function animateImgs(){
-        img_counter--;
-        console.log("counter: ", img_counter); 
-        $img = $('.lg-logo image');
-        $img.eq(img_counter).animate({
-          'opacity':0
-        },300);
-        // curr_img.attr('xlink:href', '../intro_imgs/img_'+img_counter+'.jpg' );
-      }
-    },
-
-    updatePageContent: function () {
-        HR.updatePageName();
-        HR.transitionContent(HR.currPageName);
-    },
-    
-    isEmpty: function( el ){
-        return !$.trim(el.html())
-    }
   };
 })(jQuery); // end SEF
 
