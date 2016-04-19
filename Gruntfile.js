409339be539597dca3f44f17fa3505b60ec25571
aliya robinson <aliya robinson>
2016-04-19 11:52:42 -0400

    added sass folder and updated gruntfile with sass and watch tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,12 +49,52 @@ module.exports = function(grunt) {
 				'force': false,
 				'no-write': false
 			}
-		}
+		},
+		sass: {
+			task: {
+				src: ['sass/site.scss'], 
+				dest: 'css/style.css'
+			},
+			options: {
+				'sourcemap': 'auto',
+				'trace': false,
+				'unixNewlines': false,
+				'check': false,
+				'style': 'nested',
+				'precision': 3,
+				'quiet': false,
+				'compass': false,
+				'debugInfo': false,
+				'lineNumbers': false,
+				'loadPath': [],
+				'require': [],
+				'cacheLocation': '.sass-cache',
+				'noCache': false,
+				'bundleExec': false,
+				'update': false
+			}
+		},
+		watch: {
+            src: {
+              options:{
+              	livereload:{
+			        host: 'localhost',
+			        port: 80
+			    }
+  			  },
+              files: ['sass/*.scss'], 
+              tasks: ['sass']
+            },
+        }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['clean', 'concat', 'uglify']);
+	grunt.registerTask('go', ['watch']);
+	
 };
